c1cfbdbef85aa7272556fe4aef9c8e8924c23a88
Alexfreeser <51165955+Alexfreeser@users.noreply.github.com>
2019-08-05 10:07:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/testemployees/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.testemployees;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.example.testemployees.Adapters.EmployeeAdapter;
+import com.example.testemployees.api.ApiFactory;
+import com.example.testemployees.api.ApiService;
+import com.example.testemployees.pojo.Employee;
+import com.example.testemployees.pojo.EmployeeResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewEmployees;
+    private EmployeeAdapter adapter;
+    private EmployeeViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerViewEmployees = findViewById(R.id.recyclerViewEmployees);
+        adapter = new EmployeeAdapter();
+        adapter.setEmployees(new ArrayList<Employee>());
+        recyclerViewEmployees.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewEmployees.setAdapter(adapter);
+        viewModel = ViewModelProviders.of(this).get(EmployeeViewModel.class);
+        viewModel.getEmployees().observe(this, new Observer<List<Employee>>() {
+            @Override
+            public void onChanged(@Nullable List<Employee> employees) {
+                adapter.setEmployees(employees);
+            }
+        });
+        viewModel.getErrors().observe(this, new Observer<Throwable>() {
+            @Override
+            public void onChanged(@Nullable Throwable throwable) {
+                if (throwable != null) {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    viewModel.clearErrors();
+                }
+            }
+        });
+        viewModel.loadData();
+    }
+
+}
